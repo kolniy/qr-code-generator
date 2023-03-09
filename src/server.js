19243fd419dd22bcdb26a19abbb7eaaef2bcb287
kolawole olaniyi <kolaniyi3@gmail.com>
2023-03-09 21:37:18 +0100

    qr code generator feature completed and display page showing code

--- src/server.js
+++ src/server.js
@@ -8,12 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 7000;
 
 app.set("view engine", "ejs");
-app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../public/css/style.css")));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.use("/api/v1/qr", qrRoute);
+app.use("/qr", qrRoute);
 app.listen(PORT, () => console.log(`App is Listening on port ${PORT}`));
